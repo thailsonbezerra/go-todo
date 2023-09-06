f8fde798c63fe1b2066a3f7061b60a16305f1fc8
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-09-05 23:25:25 -0300

    feat: conexão com o banco de dados estabelecida

---
+++ database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConn() (*sql.DB, error) {
+	connStr := "host=localhost port=8432 user=postgres password=1234 dbname=postgres sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/thailsonbezerra/go-todo/database"
 )
 
 func main() {
-	fmt.Println("Primeiro contato com GO!")
+	db, err := database.OpenConn()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Conexão com o banco de dados bem-sucedida!")
 }
