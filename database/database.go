d862e75fb61105bf9758831e0cef73542096ec85
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-09-07 23:01:41 -0300

    feat: utilizando variaveis de ambiente para fazer a conexao com o banco de dados

--- database/database.go
+++ database/database.go
@@ -2,12 +2,27 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func OpenConn() (*sql.DB, error) {
-	connStr := "host=db port=5432 user=postgres password=1234 dbname=postgres sslmode=disable"
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar as variáveis de ambiente")
+}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_DATABASE"),
+	)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.0
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.0.0-20190320223903-b7391e95e576 // indirect
 	mellium.im/sasl v0.3.1 // indirect
