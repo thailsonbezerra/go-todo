4af882f87966ac3a6dfd1bae6901a946c786a136
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-09-11 14:54:13 -0300

    feat: inserindo um TODO no banco de dados

--- src/controllers/todos/todos.go
+++ src/controllers/todos/todos.go
@@ -1,11 +1,28 @@
-package controllers
+package todos
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/thailsonbezerra/go-todo/src/config/database"
+	"github.com/thailsonbezerra/go-todo/src/models/todos"
 )
 
 func CriarTodo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando um TODO!"))
+
+	var t todos.Todo
+	json.NewDecoder(r.Body).Decode(&t)
+
+	db := database.GetDB()
+
+	err := db.QueryRow("INSERT INTO todos (title, description) VALUES ($1, $2) RETURNING id", t.Title, t.Description).Scan(&t.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(t)
+
 }
 
 func BuscarTodos(w http.ResponseWriter, r *http.Request) {
--- src/router/routes.go
+++ src/router/routes.go
@@ -2,13 +2,13 @@ package router
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/thailsonbezerra/go-todo/src/controllers"
+	"github.com/thailsonbezerra/go-todo/src/controllers/todos"
 )
 
 func Router(r *mux.Router) {
-	r.HandleFunc("/todos", controllers.CriarTodo).Methods("POST")
-	r.HandleFunc("/todos", controllers.BuscarTodos).Methods("GET")
-	r.HandleFunc("/todos/{id}", controllers.BuscarTodo).Methods("GET")
-	r.HandleFunc("/todos/{id}", controllers.AtualizarTodo).Methods("PUT")
-	r.HandleFunc("/todos/{id}", controllers.DeletarTodo).Methods("DELETE")
+	r.HandleFunc("/todos", todos.CriarTodo).Methods("POST")
+	r.HandleFunc("/todos", todos.BuscarTodos).Methods("GET")
+	r.HandleFunc("/todos/{id}", todos.BuscarTodo).Methods("GET")
+	r.HandleFunc("/todos/{id}", todos.AtualizarTodo).Methods("PUT")
+	r.HandleFunc("/todos/{id}", todos.DeletarTodo).Methods("DELETE")
 }
