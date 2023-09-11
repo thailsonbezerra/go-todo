fb1cc4ea0d38f1b21b1e31f1560e0064685ea337
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-09-11 15:41:32 -0300

    feat: desenvolvido as func de buscar todos os todos e buscar apenas um todo por id

--- src/controllers/todos/todos.go
+++ src/controllers/todos/todos.go
@@ -5,32 +5,65 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/thailsonbezerra/go-todo/src/config/database"
-	"github.com/thailsonbezerra/go-todo/src/models/todos"
+	modelTodo "github.com/thailsonbezerra/go-todo/src/models/todos"
 )
 
 func CriarTodo(w http.ResponseWriter, r *http.Request) {
 
-	var t todos.Todo
-	json.NewDecoder(r.Body).Decode(&t)
+	var todo modelTodo.Todo
+	json.NewDecoder(r.Body).Decode(&todo)
 
 	db := database.GetDB()
 
-	err := db.QueryRow("INSERT INTO todos (title, description) VALUES ($1, $2) RETURNING id", t.Title, t.Description).Scan(&t.ID)
+	err := db.QueryRow("INSERT INTO todos (title, description) VALUES ($1, $2) RETURNING id", todo.Title, todo.Description).Scan(&todo.ID)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	json.NewEncoder(w).Encode(t)
+	json.NewEncoder(w).Encode(todo)
 
 }
 
 func BuscarTodos(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("buscando todos os TODOs!"))
+	db := database.GetDB()
+
+	rows, err := db.Query("SELECT * FROM todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var todos []modelTodo.Todo
+	for rows.Next() {
+
+		var todo modelTodo.Todo
+		if err := rows.Scan(&todo.ID, &todo.CreatedAt, &todo.Title, &todo.Description, &todo.Completed); err != nil {
+			log.Fatal(err)
+		}
+		todos = append(todos, todo)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(todos)
+
 }
 
 func BuscarTodo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando um TODO!"))
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var todo modelTodo.Todo
+	db := database.GetDB()
+	err := db.QueryRow("SELECT * FROM todos WHERE id = $1", id).Scan(&todo.ID, &todo.CreatedAt, &todo.Title, &todo.Description, &todo.Completed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(todo)
 }
 
 func AtualizarTodo(w http.ResponseWriter, r *http.Request) {
